761b264f25344fb3939a6acda4711c811b669cee
Anna Zajac <aniazajac9112@gmail.com>
2022-01-23 23:28:35 +0100

    Fix rating

--- src/js/script.js
+++ src/js/script.js
@@ -63,6 +63,7 @@
           name: eachBook.name,
           price: eachBook.price,
           image: eachBook.image,
+          rating: eachBook.rating,
           ratingBgc: ratingBgc,
           ratingWidth: ratingWidth,
 
@@ -160,15 +161,15 @@
   
       if(rating < 6) {
   
-        background = 'background: linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
+        background = 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
   
       } else if (rating > 6 && rating <= 8) {
-        background = 'background: linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+        background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
   
       } else if(rating > 8 && rating <= 9) {
-        background = 'background: linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+        background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
       } else if(rating > 9) {
-        background = 'background: linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+        background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
       }
   
       return background;
